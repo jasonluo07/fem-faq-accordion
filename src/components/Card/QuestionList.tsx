b43b0389c9fa9c8e036b76bdcf3dccd60f466540
Jason Luo <yulaie1012@gmail.com>
2023-12-21 01:54:38 +0800

    Add QUESTIONS constant and render question list from it

--- src/components/Card/QuestionList.tsx
+++ src/components/Card/QuestionList.tsx
@@ -1,3 +1,4 @@
+import { QUESTIONS } from '@/constants';
 import Image from 'next/image';
 
 import styles from './QuestionList.module.css';
@@ -5,46 +6,15 @@ import styles from './QuestionList.module.css';
 const QuestionList = () => {
   return (
     <ul className={styles.questionList}>
-      <li className={styles.questionItem}>
-        <div className={styles.title}>
-          <h2 className={styles.text}>What is Frontend Mentor, and how will it help me?</h2>
-          <Image src="/icon-minus.svg" alt="minus" width={30} height={30} />
-        </div>
-        <p className={styles.answer}>
-          Frontend Mentor offers realistic coding challenges to help developers improve their frontend coding skills
-          with projects in HTML, CSS, and JavaScript. It’s suitable for all levels and ideal for portfolio building.
-        </p>
-      </li>
-      <li className={styles.questionItem}>
-        <div className={styles.title}>
-          <h2 className={styles.text}>Is Frontend Mentor free?</h2>
-          <Image src="/icon-plus.svg" alt="plus" width={30} height={30} />
-        </div>
-        <p className={styles.answer}>
-          Yes, Frontend Mentor offers both free and premium coding challenges, with the free option providing access to
-          a range of projects suitable for all skill levels.
-        </p>
-      </li>
-      <li className={styles.questionItem}>
-        <div className={styles.title}>
-          <h2 className={styles.text}>Can I use Frontend Mentor projects in my portfolio?</h2>
-          <Image src="/icon-plus.svg" alt="plus" width={30} height={30} />
-        </div>
-        <p className={styles.answer}>
-          Yes, you can use projects completed on Frontend Mentor in your portfolio. It&apos;s an excellent way to
-          showcase your skills to potential employers!
-        </p>
-      </li>
-      <li className={styles.questionItem}>
-        <div className={styles.title}>
-          <h2 className={styles.text}>How can I get help if I&apos;m stuck on a challenge?</h2>
-          <Image src="/icon-plus.svg" alt="plus" width={30} height={30} />
-        </div>
-        <p className={styles.answer}>
-          The best place to get help is inside Frontend Mentor&apos;s Discord community. There&apos;s a help channel
-          where you can ask questions and seek support from other community members.
-        </p>
-      </li>
+      {QUESTIONS.map(question => (
+        <li key={question.key} className={styles.questionItem}>
+          <div className={styles.title}>
+            <h2 className={styles.text}>{question.title}</h2>
+            <Image src="/icon-plus.svg" alt="plus" width={30} height={30} />
+          </div>
+          <p className={styles.answer}>{question.answer}</p>
+        </li>
+      ))}
     </ul>
   );
 };
---
+++ src/types/index.ts
@@ -0,0 +1,5 @@
+export interface IQuestion {
+  title: string;
+  key: string;
+  answer: string;
+}

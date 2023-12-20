4037a8fd4a2af393f64db6b48113a43ffc87a5cc
Jason Luo <yulaie1012@gmail.com>
2023-12-20 23:22:54 +0800

    Create header to card component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,17 @@
+import Image from 'next/image';
+
 import styles from './page.module.css';
 
 const Home = () => {
   return (
     <main className={styles.main}>
       <div className={styles.backgroundPattern} />
-      <div className={styles.card}></div>
+      <div className={styles.card}>
+        <div className={styles.header}>
+          <Image src="/icon-star.svg" alt="star" width={24} height={24} />
+          <h1 className={styles.title}>FAQs</h1>
+        </div>
+      </div>
     </main>
   );
 };

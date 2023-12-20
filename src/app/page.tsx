264400d513528cd6459891d692140a3ebfb9d8aa
Jason Luo <yulaie1012@gmail.com>
2023-12-20 22:46:57 +0800

    Add background pattern

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import styles from './page.module.css';
 const Home = () => {
   return (
     <main className={styles.main}>
-      
+      <div className={styles.backgroundPattern} />
     </main>
   );
 };

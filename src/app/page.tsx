6271d59fb69e47413f0ad87c613ab8ec05bdc200
Jason Luo <yulaie1012@gmail.com>
2023-12-21 00:13:24 +0800

    Add Card component to page module

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,4 @@
-import QuestionList from '@/components/QuestionList';
-import Image from 'next/image';
+import Card from '@/components/Card';
 
 import styles from './page.module.css';
 
@@ -7,13 +6,7 @@ const Home = () => {
   return (
     <main className={styles.main}>
       <div className={styles.backgroundPattern} />
-      <div className={styles.card}>
-        <div className={styles.header}>
-          <Image src="/icon-star.svg" alt="star" width={24} height={24} />
-          <h1 className={styles.title}>FAQs</h1>
-        </div>
-        <QuestionList />
-      </div>
+      <Card />
     </main>
   );
 };
---
+++ src/components/Card/index.tsx
@@ -0,0 +1,18 @@
+import Image from 'next/image';
+
+import styles from './Card.module.css';
+import QuestionList from './QuestionList';
+
+const Card = () => {
+  return (
+    <div className={styles.card}>
+      <div className={styles.header}>
+        <Image src="/icon-star.svg" alt="star" width={24} height={24} />
+        <h1 className={styles.title}>FAQs</h1>
+      </div>
+      <QuestionList />
+    </div>
+  );
+};
+
+export default Card;

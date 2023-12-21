b6915d5b69f4617445f95e9b34884c676e8603ac
Jason Luo <yulaie1012@gmail.com>
2023-12-21 11:24:51 +0800

    Refactor displayIcon variable assignment in QuestionList component

--- src/components/Card/QuestionList.tsx
+++ src/components/Card/QuestionList.tsx
@@ -13,6 +13,7 @@ const QuestionList = () => {
     <ul className={styles.questionList}>
       {QUESTIONS.map(({ key, title, answer }) => {
         const isOpen = openQuestions.has(key);
+        const displayIcon = isOpen ? 'minus' : 'plus';
 
         const handleToggle = () => {
           const newOpenQuestions = new Set(openQuestions);
@@ -24,11 +25,7 @@ const QuestionList = () => {
           <li key={key} className={styles.questionItem}>
             <div className={styles.header} onClick={handleToggle}>
               <h2 className={styles.title}>{title}</h2>
-              {isOpen ? (
-                <Image src="/icon-minus.svg" alt="minus" width={30} height={30} />
-              ) : (
-                <Image src="/icon-plus.svg" alt="plus" width={30} height={30} />
-              )}
+              <Image src={`/icon-${displayIcon}.svg`} alt={displayIcon} width={30} height={30} />
             </div>
             {isOpen && <p className={styles.answer}>{answer}</p>}
           </li>
